1607c5afc20aae59ddba0ee388dbd655adc110c4
Jordan Coderre <52050360+coderrej@users.noreply.github.com>
2019-08-23 13:54:55 -0400

    Add files via upload

---
+++ Delete Files Over 5 Days Old.ps1
@@ -0,0 +1,18 @@
+﻿#
+# This will display files in a directory that haven't been modified in the last 5 days, path specified with -path parameter.
+# Adding -delete parameter when calling script will remove files.
+#
+
+param (
+    [string]$path = $(throw "Please designate directory with -path parameter"),
+    [switch]$delete
+)
+
+# Gets any files not modified in the last 5 days, filters name and LastWriteTime for output
+Get-ChildItem -path $path | where { $_.LastWriteTime -le (Get-Date).AddDays(-(5)) } | Select Name,LastWriteTime | Sort-Object LastWriteTime
+
+
+# Deletes files if -delete parameter is used
+if ($delete) {
+   Get-ChildItem -path $path | Remove-Item
+}
